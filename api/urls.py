f51ebecfa3d62d2ef53fef6f79d1f615dbaf73b5
AmandaIreri <amandairerii@gmail.com>
2024-07-22 15:18:44 +0300

    added methods

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StudentListView, ClassPeriodListView, CourseListView, TeacherListView, ClassroomListViews, StudentDetailView
+from .views import StudentListView, ClassPeriodListView, CourseListView, TeacherListView, ClassroomListViews, StudentDetailView, ClassPeriodDetailView, CourseDetailView, ClassroomDetailView, TeacherDetailView
 
 
 urlpatterns = [
@@ -7,6 +7,11 @@ urlpatterns = [
     path("classperiods/", ClassPeriodListView.as_view(), name="class_period_list_view"),
     path("courses/", CourseListView.as_view(), name="course_list_view"),
     path("teachers/", TeacherListView.as_view(), name="teacher_list_view"),
-    path("Classroom/",ClassroomListViews.as_view(),name = "student_class_list_view"),
+    path("classroom/",ClassroomListViews.as_view(),name = "class_room_list_view"),
     path("students/<int:id>/", StudentDetailView.as_view(), name="student_detail_view"),
+    path("classperiods/<int:id>/", ClassPeriodDetailView.as_view(), name="class_period_detail_view"),
+    path("courses/<int:id>/", CourseDetailView.as_view(), name="course_detail_view"),
+    path("teachers/<int:id>/", TeacherDetailView.as_view(), name="teacher_detail_view"),
+    path("classroom/<int:id>/", ClassroomDetailView.as_view(), name="class_room_detail_view"),
+
 ]
--- class_period/models.py
+++ class_period/models.py
@@ -4,10 +4,8 @@ from course.models import Course
 
 # Create your models here.
 class ClassPeriod (models.Model):
-    start_time = models.DateField()
-    end_time = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, related_name='course_period')
-    course = models.CharField(max_length=20)
-    classroom = models.CharField(max_length=20)
     day_of_the_week = models.CharField(max_length=20)
     classroom = models.ForeignKey(Classroom, on_delete = models.SET_NULL, null = True, related_name ='class_time' )
